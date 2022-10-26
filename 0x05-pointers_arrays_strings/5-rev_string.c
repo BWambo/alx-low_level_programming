7cfa1581ed864aa76bbff5567e2650e43e2a8a63
BWambo <gichuguwambui@gmail.com>
2022-10-26 06:40:20 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,13 @@
 
 void rev_string(char *s)
 {
-	char *start = s;
-	char temp;
-	
-	for (i - 0; i < c; i++)
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
 		c--;
 		rv = s[i];
